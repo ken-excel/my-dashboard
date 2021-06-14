caa451d31ec5c0dc44ecae38afbade95a8bb83a6
ken-excel <isolate.xcl@gmail.com>
2021-06-14 11:05:42 +0900

    change allowed hosts

--- my_dashboard/settings.py
+++ my_dashboard/settings.py
@@ -22,13 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'my-dashboard.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'my-dashboard-heroku.herokuapp.com']
 
 
 # Application definition
