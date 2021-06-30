c6206776ad7c1422b7e0d53e708e121924c9a730
ken-excel <isolate.xcl@gmail.com>
2021-07-01 01:23:56 +0900

    Update primary key migration

--- dashboard/migrations/0001_initial.py
+++ dashboard/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name='Computer',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=50)),
+                ('name', models.CharField(max_length=50, primary_key=True)),
                 ('type', models.CharField(max_length=50)),
             ],
         ),
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 class Computer(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     type = models.CharField(max_length=50)
     def __str__(self):
         return self.name
 
 class Temperature(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     computer = models.ForeignKey(Computer, on_delete=models.CASCADE)
     temperature = models.IntegerField(default=0)
     def __str__(self):
         return self.name
 
 class Fan(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     computer = models.ForeignKey(Computer, on_delete=models.CASCADE)
     speed = models.IntegerField(default=0)
     def __str__(self):
