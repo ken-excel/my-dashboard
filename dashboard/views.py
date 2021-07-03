e46d3269b3ade59e4ebbe49bf7b7f7a0b382d763
ken-excel <isolate.xcl@gmail.com>
2021-07-03 21:21:06 +0900

    Edit crypto view and html

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,8 +3,10 @@ from django.views import generic
 
 from .models import Computer, Crypto
 
+import os
 import urllib.request
 import json
+import logging
 
 class PCMonitorView(generic.ListView):
     template_name = 'dashboard/pcmonitor.html'
@@ -19,13 +21,12 @@ class CryptoView(generic.ListView):
 
     def get_queryset(self):
         symbol_list = []
-        for crypto_obj in Crypto.objects:
+        for crypto_obj in Crypto.objects.all():
             symbol_list.append(crypto_obj.name)
         symbols = ','.join(symbol_list)
-        return fetchAPIdata(symbols)
-
-    def fetchAPIdata(symbols):
         api_key = os.environ.get('LUNARCRUSH_API_KEY')
         url = "https://api.lunarcrush.com/v2?data=assets&key=" + api_key + "&symbol=" + symbols
-        return json.loads(urllib.request.urlopen(url).read())
+        assets = json.loads(urllib.request.urlopen(url).read())
+        logging.debug(assets['data'])
+        return assets['data']
         
