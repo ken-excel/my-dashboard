cb51dd50d417202c12a81a63151f65113fab29d4
ken-excel <isolate.xcl@gmail.com>
2021-07-11 02:01:16 +0900

    Add crypto and spotify to admin page

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Computer, Temperature, Fan
+from .models import Computer, Temperature, Fan, Crypto, Spotify
 
 admin.site.register(Computer)
 admin.site.register(Temperature)
 admin.site.register(Fan)
+admin.site.register(Crypto)
+admin.site.register(Spotify)
